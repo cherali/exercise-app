1d088d50bb38a6f258a292c3234335ef32b6b471
alich6 <alicheraghi148@gmail.com>
2023-01-30 11:19:38 +0330

    fix typo

--- src/providers/HTMLTagProvider/useHTMLTag.ts
+++ src/providers/HTMLTagProvider/useHTMLTag.ts
@@ -2,4 +2,4 @@ import { useContext } from 'react'
 import { HTMLTagProviderContext } from './HTMLTagProviderContext'
 import type { HTMLTagProviderContextProps } from './index.d'
 
-export const useTheme = (): HTMLTagProviderContextProps => useContext(HTMLTagProviderContext)
+export const useHTMLTag = (): HTMLTagProviderContextProps => useContext(HTMLTagProviderContext)
